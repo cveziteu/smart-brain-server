dbceed50b7a11ac8a6cd046f2d0927fd2d076f7e
Constantin Veziteu <c.veziteu@gmail.com>
2019-07-27 23:10:10 +0300

    Developer update #2 - 27.07.2019
    1) Added PostgreSQL database <3
    2) Added Knex.js to help us with our database queries
    3) Updated server method get/
    4) Updated server method post/login
    5) Updated server method post/register
    6) Updated server method get/profile/:id
    7) Updated server method put/image
    9) Added server method post/imagenoface

--- server.js
+++ server.js
@@ -1,92 +1,168 @@
+// ExpressJS
 const express = require('express');
+
+// Body Parser
 const bodyParser = require('body-parser');
+
+// CORS
 const cors = require('cors');
 
+//Knex
+const knex = require('knex');
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'admin',
+        password: 'test',
+        database: 'smart-brain'
+    }
+});
+
+// bcrypt
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// Main app
 const app = express();
+// adding bodyparser and CORS to our app
 app.use(bodyParser.json());
 app.use(cors());
 
-const db = {
-    users: [
-        {
-            id: '1',
-            username: 'Constantin',
-            email: 'c.veziteu@gmail.com',
-            password: '123',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '2',
-            username: 'Laura',
-            email: 'laura.veziteu@gmail.com',
-            password: '1234',
-            entries: 0,
-            joined: new Date()
-        },
-    ]
-}
 
+//!                                                                                                 SERVER COMMUNICATION                    
 
+//?                                                                                                 /
 app.get('/', (request, response) => {
-    response.json(db.users);
-})
-
-app.post('/login', (request, response) => {
-    if (request.body.email === db.users[0].email &&
-        request.body.password === db.users[0].password) {
-            // response.json('success');
-            response.json(db.users[0]);
-    }
-    else {
-        response.status(400).json('error');
-    }
-})
-
-app.post('/register', (request, response) => {
-    const { email, username, password} = request.body;
-
-    db.users.push({
-        id: '3',
-        username: username,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+    db('users').then(data => {
+        response.json(data);
     })
-    response.json(db.users[db.users.length-1])
 })
 
+//?                                                                                                 /login
+app.post('/login', (request, response) => {
+    const { email, password } = request.body;
+    db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => bcrypt.compare(password, data[0].hash))
+        .then(result => {
+            if (result) {
+                db('users')
+                    .where('email', '=', email)
+                    .then(user => response.json(user[0]))
+                    .catch(err => {
+                        response.json.status(400).json('unable to get user')
+                    })
+            }
+            else {
+                response.status(400).json("wrong credentials");
+            }
+        })
+        .catch(err => response.status(400).json('bad request'));
+})
+
+
+//?                                                                                                 /register
+app.post('/register', (request, response) => {
+    const { email, username, password } = request.body;
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        db.transaction(trx => {
+            trx('login')
+                .insert({
+                    hash: hash,
+                    email: email
+                })
+                .returning('email')
+                .then(loginEmail => {
+                    return trx('users')
+                        .returning('*')
+                        .insert({
+                            email: loginEmail[0],
+                            username: username,
+                            joined: new Date()
+                        })
+                        .then(user => {
+                            response.json(user[0]);
+                        })
+                })
+                .then(trx.commit)
+                .catch(trx.rollback)
+        })
+        .catch(err => {
+            response.status(400).json('unable to register')
+        })
+    })
+})
+
+
+//?                                                                                                 /profile/:id
 app.get('/profile/:id', (request, response) => {
     const {id} = request.params;
-    let found = false;
 
-    db.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return response.json(user);
-        }
-    })
-    if (!found) {
-        response.status(404).json('user not found');
-    }
+    db('users').where({id})
+        .then(user => {
+            if (user.length > 0) {
+                response.json(user[0])
+            }
+            else {
+                response.status(400).json('not found')
+            }
+        }) 
+        .catch(err => response.status(400).json('error'))        
+
 })
 
+
+//?                                                                                                 /image
 app.put('/image', (request, response) => {
-    const {id} = request.body;
-    let found = false;
-
-    db.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return response.json(user);
-        }
+    const {id, imageUrl} = request.body;
+    db.transaction(trx => {
+        trx('entries')
+            .insert({
+                userId: id,
+                entry: imageUrl,
+                posted: new Date()
+            })
+            .returning('userId')
+            .then(userId => {
+                return trx('users')
+                    .where('id', '=', userId[0])
+                    .increment('entries', 1)
+                    .returning('entries')
+                    .then(entries => {
+                        response.json(entries[0]);
+                    })
+                    .catch(err => response.status(400).json('unable to fetch entries'));
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
     })
-    if (!found) {
-        response.status(404).json('not found');
-    }
+    .catch(err => {
+        response.status(400).json('Error - something went wrong.')
+    })
+    
 })
+
+
+//?                                                                                                 /imagenoface
+app.post('/imagenoface', (request, response) => {
+    const {id, imageUrl} = request.body;
+    db('entries')
+        .insert({
+            userId: id,
+            entry: imageUrl,
+            posted: new Date()
+        })
+        .then(
+            response.json('success')
+        )
+        .catch(
+            response.status(400).json('failed')
+        );
+})
+
+
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
